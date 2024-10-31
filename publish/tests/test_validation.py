db0f7e28ea8ab959528ff4b182fc5f1d5d3d3cfd
Vidhisha Kamat <vids.kamat@gmail.com>
2024-10-31 16:14:53 -0400

    Fix: test_validation

--- publish/tests/test_validation.py
+++ publish/tests/test_validation.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 from django.test import SimpleTestCase
-from ..views import has_date_passed
+from utilities import DateUtils
 
 class DateValidationTests(SimpleTestCase):
     def test_past_date(self):
-        self.assertTrue(has_date_passed("2020-01-01"), "Should be False for past date")
+        self.assertTrue(DateUtils.has_date_passed("2020-01-01"), "Should be False for past date")
 
     def test_current_date(self):
         current_date = datetime.today().date().strftime("%Y-%m-%d")
-        self.assertFalse(has_date_passed(current_date), "Should be True for current date")
+        self.assertFalse(DateUtils.has_date_passed(current_date), "Should be True for current date")
 
     def test_future_date(self):
-        self.assertFalse(has_date_passed("2025-01-01"), "Should be True for future date")
+        self.assertFalse(DateUtils.has_date_passed("2025-01-01"), "Should be True for future date")
