1209db198f6efba77ebde893171518d4d50f3378
Vidhisha Kamat <vids.kamat@gmail.com>
2024-10-31 15:52:51 -0400

    Edit: views.py

--- publish/views.py
+++ publish/views.py
@@ -26,13 +26,7 @@ mapsService = None
 secrets = Secrets()
 urlConfig = URLConfig()
 
-# TODO : Remove this
-def has_date_passed(date: str) -> bool: 
-    given_date = datetime.strptime(date, "%Y-%m-%d").date()
-    
-    today = datetime.today().date()
-    
-    return given_date < today
+
 
 def intializeDB():
     """
